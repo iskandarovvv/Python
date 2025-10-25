901d2251c43491a8df9c3bec38e6fb99eb58f55e
iskandarovvv <aiskandarov641@gmail.com>
2025-10-25 18:03:48 +0500

    Python
    Masalalar

---
+++ 2.py
@@ -0,0 +1,190 @@
+import math
+
+#15-masala
+#a = int(input("A = "))
+#b = int(input("B = "))
+#c = int(input("C = "))
+#if a + b + c :
+#    print(a)
+#elif b + a +c :
+#    print(b)
+#elif c + a + b :
+#    print(c)
+
+
+
+
+
+#16-masala
+#A = int(input("A = "))
+#B = int(input("B = "))
+#C = int(input("C = "))
+#if A < B < C :
+#    A = 2 * A
+#    B = 2 * B
+#    C = 2 * C
+#    print(f"={A} , ={B} , ={C} .")
+#else :
+#    A = -A
+#    B = -B
+#    C = -C
+#    print(f"={A}, ={B}, ={C}.")
+
+
+
+
+
+
+#17-masala
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#if A < B < C :
+#    A = 2 * A
+#    B = 2 * B
+#    C = 2 * C
+#elif A > B > C :
+#    A = 2 * A
+#    B = 2 * B
+#    C = 2 * C
+#else :
+#    A = -A
+#    B = -B
+#    C = -C
+#print(f"={A}, ={B}, ={C}.")
+
+
+
+
+
+
+#18-masala
+#a = int(input("A = "))
+#b = int(input("B = "))
+#c = int(input("C = "))
+#if a == b :
+#    print(3)
+#elif a == c :
+#    print(2)
+#else :
+#    print(1)
+#
+
+
+
+
+
+
+#19-masala
+#a = int(input("A = "))
+#b = int(input("B = "))
+#c = int(input("C = "))
+#d = int(input("D = "))
+#if a == b :
+#    print(4)
+#elif a == c :
+#    print(3)
+#elif a == d :
+#    print(2)
+#else :
+#    print(1)
+
+
+
+
+
+#20-masala
+#A = int(input("A = "))
+#B = int(input("B = "))
+#C = int(input("C = "))
+#masofaA_B = abs(A - B)
+#masofaA_C = abs(A - C)
+#if masofaA_B < masofaA_C :
+#    print(f"A nuqtaga eng yaqin nuqta B va ular orasidagi masofa: {masofaA_B}")
+#else :
+#     print(f"A nuqtaga eng yaqin nuqta C va ular orasidagi masofa: {masofaA_C}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#24-masala
+#x = int(input("X ni kiriting :  "))
+#if x > 0 :
+#    f = 2 * math.sin(x)
+#else :
+#    f = x - 6
+#print(f"f(x) = {f}")
+
+
+
+
+
+
+
+
+#25-masala
+#x = int(input("X ni kiriting :  "))
+#if x < - 2 and x > 2 :
+#    f = 2 * x
+#else :
+#    f = - 3 * x 
+#print(f"f(x) = {f}")
+
+
+
+
+
+
+
+
+
+#26-masala
+#x = int(input("X ni kiriting :  "))
+#if x <= 0 :
+#    f = - x
+#elif 0 < x < 2 :
+#    f = x**2
+#else :
+#    f = 4
+#print(f"f(x) = {f}")
+
+
+
+
+
+
+#29-masala
+#a = int(input("A ni kiriting :  "))
+#if a > 0 and a % 2 != 0 :
+#    print(f"Musbat toq son {a}")
+#elif a < 0 and a % 2 == 0 :
+#    print(f"Manfiy juft son {a}")
+#else :
+#    print("Son 0 ga teng")
+
+
+
+
+
+
+
+#30-masala
+#a = int(input("1 dan 999 gacha son kiriting :  "))
+#if 10 <= a <= 99 :
+#    f = a % 2 == 0
+#    print(f"{a} ikki xonali juft son")
+#elif 100 <= a <= 999 :
+#    f = a % 2 == 0 
+#    print(f"{a} uch xonali juft son")
+#else : 
+#    print("Uch xonali toq son")
