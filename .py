071b4c62c1427c2c4dd079929960db692505bb3d
iskandarovvv <aiskandarov641@gmail.com>
2025-10-28 17:02:47 +0500

    python
    Yangi

---
+++ .py
@@ -0,0 +1,200 @@
+# 1- masala
+#a = float(input("a = "))
+#p = 4 * a
+#print( "tomoni" , a ,"ga teng bolgan kvadratning premetri", p)
+ 
+
+
+
+
+#2-masala 
+#p = float(input("a = "))
+#S = 4*4
+#print(S)
+
+
+
+
+
+#3-masal
+#a=float(input("a = "))
+#p=float(input("p = "))
+#S = a*p
+#P  = 2*(a+p)
+#print("yuzasi ", a, "parametri", p) 
+
+
+
+#4-masala
+#d=float(input("d = "))
+#Pi = 3.14
+#L = Pi*d
+#print(L)
+
+
+
+
+#5-masala
+#a = float(input("a = "))
+#V =6*a**3
+#S = 6*a**2
+#print("kubning tomoni",V,"uning hajmi", S)
+
+
+
+
+#6-masala
+#a = float(input("a = "))
+#b = float(input("b = "))
+#c = float(input("c = "))
+#V = a*b*c
+#S=2*(a*b+b*c+a*c)
+#print("hajmi",V, "tola sirti", S)
+
+
+#7-masala
+#R = float(input("R = "))
+#Pi = 3.14
+#L = 2*Pi * R
+#S = Pi * R*R
+#print(L,S)
+
+
+
+
+
+
+
+
+
+#8-masala
+#a = float(input("a = "))
+#b = float(input("b = "))
+#A = (a+b) ** 0.5
+#print(A)
+
+
+
+
+
+#9-masala 
+#a = float(input("a ="))
+#b = float(input("b ="))
+#B = (a * b) ** 0.5
+#input(B)
+
+
+
+
+
+#10-masala
+#a=float(input("a= "))
+#b = float(input("b = "))
+#d = a+b 
+#e = a-b
+#f = a*a , b*b
+#print(d, e, f)
+
+
+
+# 11-masala
+#a = float(input("a = "))
+#b = float(input("b = "))
+#print(a + b)       # yig'indi
+#print(a * b)       # ko'paytma
+#print(abs(a), abs(b))  # modullari
+
+
+
+
+# 12-masala
+#a = float(input("a = "))
+#b = float(input("b = "))
+#c = (a*a + b*b) ** 0.5
+#P = a + b + c
+#print(c, P)
+
+
+
+
+# 13-masala
+#pi = 3.14
+#R1 = float(input("R1 = "))
+#R2 = float(input("R2 = "))
+#S1 = pi * R1 * R1
+#S2 = pi * R2 * R2
+#print(S1, S2, S1 - S2)
+
+
+
+# 14-masala
+#L = float(input("L = "))
+#pi = 3.14
+#R = L / (2 * pi)
+#S = pi * R * R
+#print(R, S)
+
+
+
+
+# 15-masala
+#S = float(input("S = "))
+#pi = 3.14
+#R = (S / pi) ** 0.5
+#d = 2 * R
+#print(R, d)
+
+
+
+
+# 16-masala
+#x1 = float(input("x1 = "))
+#x2 = float(input("x2 = "))
+#print(abs(x2 - x1))
+
+
+
+
+
+
+#17-masala
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#print(abs(C - A) + abs(C - B))
+
+
+
+
+
+#18-masala
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#print(abs(C - A) * abs(C - B))
+
+
+
+
+
+#x1 = f
+#19-masloat(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#print(2 * (abs(x2 - x1) + abs(y2 - y1)))
+#print(abs(x2 - x1) * abs(y2 - y1))
+
+
+
+#20-masala 
+#x1 = float(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#print(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
+
+
+
+
+#21-masala
