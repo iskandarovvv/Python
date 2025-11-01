cf83858573ae47b5408fe0992922194f7c6f96fe
isroil <isroilsaparboev@gmail.com>
2025-11-01 16:50:57 +0500

    isroil.py

--- list.py.py
+++ list.py.py
@@ -3,3 +3,31 @@
 
 
 # 3
+#sonlar = [12, -7 , 3.14, 0, -2.5, 100, 8.9]
+#print(sonlar)
+
+
+
+#4
+#sonlar = [12, -7 , 3.14, 0, -2.5, 100, 8.9]
+#print(sonlar[0]+ 10)
+
+
+
+
+#5
+#t_shaxs = ["Alisher Navoi", "Amir Temur" , "Imom Al Buxoriy"]
+#z_shaxs = ["Stev Jobs" , "Bill Gets"]
+#print(f"Tarixiy shaxs {t_shaxs[0]} Zamonaviy shaxs {z_shaxs[0]}")
+
+
+
+
+#6
+#t_shaxslar = ["Alisher Navoi", "Amir Temur" , "Imom Al Buxoriy"]
+#z_shaxslar = ["Stev Jobs" , "Bill Gets"]
+#print(f"Tarixiy shaxs {t_shaxslar.pop(1)} Zamonaviy shaxs {z_shaxslar.pop(0)} ")
+
+
+
+
