d7b49a786584ae83e1460381e28a0fa8e168c7a2
isroil <aiskandarov641@gmail.com>
2025-11-07 16:09:23 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,52 +1,53 @@
 # Davlatlar ro'yxatini tuzamiz
-davlatlar = ["O'zbekiston", "Qozog'iston", "Tojikiston", "Qirg'iziston", "Turkmaniston", "Rossiya", "Xitoy", "AQSH", "Yaponiya", "Fransiya"]
+#davlatlar = ["O'zbekiston", "Qozog'iston", "Tojikiston", "Qirg'iziston", "Turkmaniston", "Rossiya", "Xitoy", "AQSH", "Yaponiya", "Fransiya"]
 
 # 1. Ro'yxatni konsolga chiqarish
-print("Asl ro'yxat:")
-print(davlatlar)
+#print("Asl ro'yxat:")
+#print(davlatlar)
 
 # 2. Ro'yxatning uzunligini chiqarish
-print("\nRo'yxat uzunligi:", len(davlatlar))
+#print("\nRo'yxat uzunligi:", len(davlatlar))
 
 # 3. sorted() yordamida tartiblangan ro'yxat
-print("\nAlifbo bo'yicha tartiblangan ro'yxat (sorted() yordamida):")
-print(sorted(davlatlar))
+#print("\nAlifbo bo'yicha tartiblangan ro'yxat (sorted() yordamida):")
+#print(sorted(davlatlar))
 
 # 4. sorted() yordamida teskari tartibda
-print("\nTeskari tartibda tartiblangan ro'yxat (sorted(reverse=True)):")
-print(sorted(davlatlar, reverse=True))
+#print("\nTeskari tartibda tartiblangan ro'yxat (sorted(reverse=True)):")
+#print(sorted(davlatlar, reverse=True))
 
 # 5. Asl ro'yxatni yana chiqarish (o'zgarmagan holatda)
-print("\nAsl ro'yxat (o'zgarmagan):")
-print(davlatlar)
+#print("\nAsl ro'yxat (o'zgarmagan):")
+#print(davlatlar)
 
 # 6. reverse() yordamida ro'yxatni teskari chiqarish
-davlatlar.reverse()
-print("\nreverse() yordamida teskari chiqarilgan ro'yxat:")
-print(davlatlar)
+#davlatlar.reverse()
+#print("\nreverse() yordamida teskari chiqarilgan ro'yxat:")
+#print(davlatlar)
 
 # 7. sort() yordamida ro'yxatni alifbo bo'yicha tartiblash
-davlatlar.sort()
-print("\nsort() yordamida alifbo bo'yicha tartiblangan ro'yxat:")
-print(davlatlar)
+#davlatlar.sort()
+#print("\nsort() yordamida alifbo bo'yicha tartiblangan ro'yxat:")
+#print(davlatlar)
 
 # 8. sort() yordamida teskari alifbo tartibida
-davlatlar.sort(reverse=True)
-print("\nsort() yordamida teskari alifbo tartibida:")
-print(davlatlar)
+#davlatlar.sort(reverse=True)
+#print("\nsort() yordamida teskari alifbo tartibida:")
+#print(davlatlar)
 
-#Juft sonlar ro'yxatini yaratamiz
-even_numbers = list(range(120, 1201, 2))
-n = len(even_numbers)
-head = even_numbers[:20]
-mid_start = max(0, n//2 - 10)
-mid_end = min(n, n//2 + 10)
-middle = even_numbers[mid_start:mid_end]
-tail = even_numbers[-20:]
-print("Boshi:", head)
-print("O'rtasi:", middle)
-print("Oxiri:", tail)
+# 9 Ro'yxatdagi sonlar yig'indisini hisoblang va konsolga chiqaring
+#son = list(range(120,1200))
+#print(sum(son))
 
+
+
+#10 Ro'yxatdagi eng katta va eng kichik son o'rtasidagi ayirmani hisoblang va konsolga chiqaring
+#print(max(son)-min(son))
+
+
+
+# 11 Ro'yxatdagi elementlar sonini hisoblang
+#print(len(son))
 
 
 
